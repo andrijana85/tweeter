0aa958f93e1f9e95d9d8acc80f53affa5cc32092
andrijana85 <topalovic.andrijana@yahoo.com>
2023-07-10 19:18:49 -0400

    Implement the character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+
+$(document).ready(function() {
+  console.log('is loaded');
+ 
+  $('#tweet-text').on('keyup', function(event) {
+
+    const charCounter = $(this).val.length;
+    
+  });
+});

6a457f63b6d6b8cf9bf37925e7736e75175baa13
andrijana85 <topalovic.andrijana@yahoo.com>
2023-07-10 20:23:31 -0400

    Adjust the code so the counter turns red when invalid

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,19 @@
 /* eslint-disable no-undef */
 
 $(document).ready(function() {
-  console.log('is loaded');
+  // console.log('is loaded');
  
-  $('#tweet-text').on('keyup', function(event) {
-
-    const charCounter = $(this).val.length;
+  $('#tweet-text').on('input', function(event) {
+    //calculate the length of the input value
+    const inputLength = $(this).val().length;
+    let counter = 140;
+    let charCount = counter - inputLength;
     
+    $('.counter').text(charCount);
+
+    if (charCount < 0) {
+      $('.counter').css('color', 'red');
+    }
+
   });
 });

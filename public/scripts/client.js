2bbca61da23ecd48739c89da4ac4a0d7fc475870
andrijana85 <topalovic.andrijana@yahoo.com>
2023-07-14 22:27:01 -0400

    implement createTweetElement function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,28 @@
+/* eslint-disable no-undef */
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const createTweetElement = function(tweetData) {
+  const $tweet = $(`<article class="tweet">Hello world</article>`);
+  return $tweet;
+};
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+};
+
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#all-tweets').append($tweet);

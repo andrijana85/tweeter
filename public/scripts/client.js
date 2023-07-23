fbbbb145f4bc0a96101728f76256ed2d2b9dd47e
andrijana85 <topalovic.andrijana@yahoo.com>
2023-07-23 19:34:48 -0400

    fix the code so when a user loads the page initially, there are the default tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -61,57 +61,56 @@ $(document).ready(function() {
     }
   };
 
-  //submit form
-  $('.new-form').submit(function(event) {
-    event.preventDefault(); // prevent the default form submission
-    
-    //form data into a query string
-    const tweetText = $(this).serialize();
-    const textValue = $('#tweet-text').val().trim();
-
-    if (textValue === '' || textValue === null) {
-      $('.error-short').slideDown('slow');
-      $('.error-short').css('display', 'flex');
-      return;
-    }
-    if (textValue.length > 140) {
-      $('.error-long').slideDown('slow');
-      $('.error-long').css('display', 'flex');
-      return;
-    }
-    $('.error-short').slideUp('slow');
-    $('.error-long').slideUp('slow');
-
-    //POST request
+  // fetch (GET) data from the server
+  const loadTweets = () => {
     $.ajax({
-      method:'POST',
-      url:'http://localhost:8080/tweets',
-      data: tweetText,
-      success: function() {
-        loadTweets();
-        $('#tweet-text').val("");// reset the value afte tweet is submitted
-        $('.counter').text('140');//reset the character counter
+      method: 'GET',
+      url: 'http://localhost:8080/tweets',
+      dataType: 'json',
+      success: (tweets) => {
+        renderTweets(tweets);
       },
       error: (error) => {
-        console.log("There is an error: ", error);
+        console.log("Something went wrong", error);
       }
     });
-    
+  };
+  loadTweets();
+});
 
-    // fetch (GET) data from the server
-    const loadTweets = () => {
-      $.ajax({
-        method: 'GET',
-        url: 'http://localhost:8080/tweets',
-        dataType: 'json',
-        success: (tweets) => {
-          renderTweets(tweets);
-        },
-        error: (error) => {
-          console.log("Something went wrong", error);
-        }
-      });
-    };
-    loadTweets();
+//submit form
+$('.new-form').submit(function(event) {
+  event.preventDefault(); // prevent the default form submission
+    
+  //form data into a query string
+  const tweetText = $(this).serialize();
+  const textValue = $('#tweet-text').val().trim();
+
+  if (textValue === '' || textValue === null) {
+    $('.error-short').slideDown('slow');
+    $('.error-short').css('display', 'flex');
+    return;
+  }
+  if (textValue.length > 140) {
+    $('.error-long').slideDown('slow');
+    $('.error-long').css('display', 'flex');
+    return;
+  }
+  $('.error-short').slideUp('slow');
+  $('.error-long').slideUp('slow');
+
+  //POST request
+  $.ajax({
+    method:'POST',
+    url:'http://localhost:8080/tweets',
+    data: tweetText,
+    success: function() {
+      loadTweets();
+      $('#tweet-text').val("");// reset the value afte tweet is submitted
+      $('.counter').text('140');//reset the character counter
+    },
+    error: (error) => {
+      console.log("There is an error: ", error);
+    }
   });
 });
